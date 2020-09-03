2e6663aacadaa8b0a1d5107991cff4b9c18169e9
Camryn Patterson <camrynpatterson@yahoo.com>
2020-09-03 20:56:04 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,60 +1,52 @@
-class Song 
+class Song
+attr_accessor :name, :artist, :genre
+
 @@count = 0
-  
-def self.count 
-@@count
-end 
-  
 @@genres = []
+@@artists = []
+
+def initialize(song_name, artist, genre)
+@name = song_name
+@artist = artist
+@genre = genre
+@@count += 1
+@@genres << genre
+@@artists << artist
+end
+
+def self.count
+@@count
+end
 
 def self.genres
-@@genres.uniq
+@@genres.uniq!
 end
-  
+
+def self.artists
+@@artists.uniq!
+end
+
 def self.genre_count
 genre_count = {}
 @@genres.each do |genre|
 if genre_count[genre]
-  genre_count[genre] += 1 
+genre_count[genre] += 1
 else
   genre_count[genre] = 1
 end
 end
 genre_count
 end
-  
-@@artists = []
-  
+
 def self.artist_count
-@@artists.inject(Hash.new(0)) { |total, i| total[i] += 1 ;total}
+artist_count = {}
+@@artists.each do |artist|
+if artist_count[artist]
+artist_count[artist] += 1
+else
+artist_count[artist] = 1
 end
-  
-def self.artists
-@@artists.uniq
 end
-  
-def initialize(name, artist, genre)
-@name = name
-@artist = artist
-@@artists << artist
-@genre = genre
-@@genres << genre
-@@count += 1
+artist_count
 end
-attr_accessor :name, :artist, :genre
-end 
-
-the_day = Song.new("The Day", "John Smith", "classic")
-tomorow = Song.new("Tomorow", "John Smith", "classic")
-a_yearz = Song.new("A Yearz", "Adam Johns", "oldtime")
-a_rabess = Song.new("Arabess", "Habibi Watan", "Rai")
-cus_umak = Song.new("Cus Ummach", "Haifa Wehbe", "Rai")
-
-puts the_day.name
-puts the_day.artist
-puts the_day.genre
-puts Song.count
-Song.genres
-Song.artists
-Song.genre_count
-Song.artist_count
+end
